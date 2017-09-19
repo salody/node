bc81c0006e8abef24a97b05c0e298fdf5a22753b
liuguanbang <salodyliu@gmail.com>
2017-09-19 16:18:10 +0800

    the node cli

---
+++ exercise/exercise01/example-1.js
@@ -0,0 +1,12 @@
+/**
+ * 功能描述:
+ * @author: liuguanbang
+ * 2017/9/18
+ */
+
+console.log('Hello World');
+
+console.log(left_pad('hello', 20, '1'));
+function left_pad(rightStr, num, leftBaseStr) {
+  return leftBaseStr.repeat(num) + rightStr;
+}
---
+++ exercise/exercise01/example-2.js
@@ -0,0 +1,7 @@
+/**
+ * 功能描述:
+ * @author: liuguanbang
+ * 2017/9/18
+ */
+
+process.stdout.write('Hello World');
