00071a9ef83eba8ee75003cd00ab8c5fd75dc822
liuguanbang <salodyliu@gmail.com>
2017-09-19 16:27:48 +0800

    change the tab size to 4

--- exercise/exercise01/example-1.js
+++ exercise/exercise01/example-1.js
@@ -7,6 +7,7 @@
 console.log('Hello World');
 
 console.log(left_pad('hello', 20, '1'));
+
 function left_pad(rightStr, num, leftBaseStr) {
-  return leftBaseStr.repeat(num) + rightStr;
+    return leftBaseStr.repeat(num) + rightStr;
 }
--- exercise/exercise01/index.js
+++ exercise/exercise01/index.js
@@ -5,77 +5,77 @@
  */
 
 const fs = require('fs');
-const { stdin, stdout} = process;
+const {stdin, stdout} = process;
 
 // The process.cwd() method returns the current working directory of the Node.js process.
 fs.readdir(process.cwd(), (err, files) => {
-  // 保存file的stat
-  const stats = [];
+    // 保存file的stat
+    const stats = [];
 
-  // 输入你的选择
-  const read = function () {
+    // 输入你的选择
+    const read = function () {
+        console.log('');
+        stdout.write('  \033[33mEnter your choice: \033[39m');
+        stdin.resume();
+        stdin.setEncoding('utf8');
+        stdin.on('data', option)
+    };
+
+    // 输入选择后的回调函数
+    const option = function (data) {
+        const filename = files[Number(data)];
+        if (!filename) {
+            stdout.write('  \033[31mEnter your choice: \033[39m')
+        } else {
+            stdin.pause();
+            if (stats[Number(data)].isDirectory()) {
+                fs.readdir(__dirname + '/' + filename, (err, files) => {
+                    if (err) throw err;
+                    console.log('');
+                    console.log('  (' + files.length + ' files)');
+                    files.forEach((file) => {
+                        console.log('    -  ' + file);
+                    })
+                })
+            } else {
+                fs.readFile(__dirname + '/' + filename, 'utf-8', (err, data) => {
+                    if (err) {
+                        throw new Error('读取文件错误');
+                    }
+                    // .匹配除换行符 \n 之外的任何单字符
+                    // *匹配前面的子表达式零次或多次
+                    // $n 假如第一个参数是 RegExp对象，并且 n 是个小于100的非负整数，那么插入第 n 个括号匹配的字符串
+                    console.log('\033[90m' + data.replace(/(.*)/g, '    $1') + '\033[39m');
+                })
+            }
+        }
+    };
+
     console.log('');
-    stdout.write('  \033[33mEnter your choice: \033[39m');
-    stdin.resume();
-    stdin.setEncoding('utf8');
-    stdin.on('data', option)
-  };
 
-  // 输入选择后的回调函数
-  const option = function (data) {
-    const filename = files[Number(data)];
-    if (!filename) {
-      stdout.write('  \033[31mEnter your choice: \033[39m')
-    } else {
-      stdin.pause();
-      if (stats[Number(data)].isDirectory()) {
-        fs.readdir(__dirname + '/' + filename, (err, files) => {
-          if (err) throw err;
-          console.log('');
-          console.log('  (' + files.length + ' files)');
-          files.forEach((file) => {
-            console.log('    -  ' + file);
-          })
-        })
-      } else {
-        fs.readFile(__dirname + '/' + filename, 'utf-8', (err, data) => {
-          if (err) {
-            throw new Error('读取文件错误');
-          }
-          // .匹配除换行符 \n 之外的任何单字符
-          // *匹配前面的子表达式零次或多次
-          // $n 假如第一个参数是 RegExp对象，并且 n 是个小于100的非负整数，那么插入第 n 个括号匹配的字符串
-          console.log('\033[90m' + data.replace(/(.*)/g, '    $1') + '\033[39m');
-        })
-      }
+    if (!files.length) {
+        console.log('  \033[36m No files to show!\033[39m\n');
     }
-  };
 
-  console.log('');
+    console.log('  Select witch file or directory you want to see\n');
 
-  if (!files.length) {
-    console.log('  \033[36m No files to show!\033[39m\n');
-  }
+    const file = (i) => {
+        const filename = files[i];
+        fs.stat(__dirname + '/' + filename, (err, stat) => {
+            stats.push(stat);
+            if (stat.isDirectory()) {
+                console.log('    ' + i + '  \033[36m' + filename + '\033[39m');
+            } else {
+                console.log('    ' + i + '  \033[32m' + filename + '\033[39m');
+            }
 
-  console.log('  Select witch file or directory you want to see\n');
+            if (++i === files.length) {
+                read();
+            } else {
+                file(i);
+            }
+        })
+    };
 
-  const file = (i) => {
-    const filename = files[i];
-    fs.stat(__dirname + '/' + filename, (err, stat) => {
-      stats.push(stat);
-      if (stat.isDirectory()) {
-        console.log('    ' + i + '  \033[36m' + filename + '\033[39m');
-      } else {
-        console.log('    ' + i + '  \033[32m' + filename + '\033[39m');
-      }
-
-      if (++i === files.length) {
-        read();
-      } else {
-        file(i);
-      }
-    })
-  };
-
-  file(0);
+    file(0);
 });
